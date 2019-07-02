e6a5c9fae385447e191c822266dcf996fdb76c6f
Patrick Gavigan <pgavigan@gmail.com>
2019-07-02 15:40:19 -0400

    Building up the agent project side

---
+++ src/main/java/acceleratedBDI/environment/TestRunner.java
@@ -0,0 +1,14 @@
+package acceleratedBDI.environment;
+
+import jason.architecture.AgArch;
+
+public class TestRunner {
+
+    public static void main( String[] args ) {
+        System.out.println("Hello world");
+
+        AgArch myAgArch = new AgArch();
+       myAgArch.toString();
+
+    }
+}
