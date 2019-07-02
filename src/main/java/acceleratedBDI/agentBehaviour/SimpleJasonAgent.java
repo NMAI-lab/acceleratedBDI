a556db09353134b12b68c3d31881443051e905d0
Patrick Gavigan <pgavigan@gmail.com>
2019-07-02 16:05:55 -0400

    A bit of code cleanup

--- src/main/java/acceleratedBDI/agentBehaviour/SimpleJasonAgent.java
+++ src/main/java/acceleratedBDI/agentBehaviour/SimpleJasonAgent.java
@@ -74,13 +74,12 @@ public class SimpleJasonAgent extends AgArch {
 		//	e.printStackTrace();
 		//}
 	}
-	
+
+	/**
+	 * Run reasoning cycles on the agent
+	 */
 	public void run(){
-		//logger.log(Level.FINE, "I'm a Jason Agent and I'm starting");
-		System.out.println("Reasoning!");
 		try {
-		//	running= true;
-			
 			while (isRunning()) {
 				// calls the Jason engine to perform one reasoning cycle
 				getTS().reasoningCycle();
@@ -88,7 +87,6 @@ public class SimpleJasonAgent extends AgArch {
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
-		System.out.println("Reasoned!");
 	}
 	
 	// this method just add some perception for the agent
@@ -96,47 +94,8 @@ public class SimpleJasonAgent extends AgArch {
 	public List<Literal> perceive() {
 		List<Literal> litteralList = new ArrayList<Literal>();
 		litteralList.add(Literal.parseLiteral("fact(5)"));
-		// This line will need to go away once the SIM side handles this.
-		//agentState.buildSnapshot();
-		
-		// Get the perceptions from agentState
-		//PerceptionSnapshot currentPerceptions = new PerceptionSnapshot(this.agentState.getPerceptions(this.lastPerceptionId));
-		//this.lastPerceptionId = currentPerceptions.getLatestTimeStamp();
-		
-		// Update the history, get the list of literals to send to the agent
-		//List<Literal> perceptionLiterals = new ArrayList<Literal>(this.perceptHistory.updatePerceptions(currentPerceptions));
-		
-		//logger.log(Level.FINE, "Agent " + getAgName() + " Perceiving perception "+ this.lastPerceptionId);
-		//logger.log(Level.FINE, perceptionLiterals.toString());
-		
-		// Write the perceptions to the perception logfile
-		//try {
-		//	BufferedWriter writer = new BufferedWriter(new FileWriter(this.perceptionLogFile, true));
-		//	for (Literal current : perceptionLiterals) {
-		//		writer.append(current.toString() + " ");
-		//	}
-		//	writer.newLine();
-		//	writer.close();
-		//} catch (IOException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		//}
-		
-		
-		//long currentSystemTime = System.currentTimeMillis();
-		//long simulationCycleTime = currentSystemTime - this.lastCycleTimeStamp;
-		//this.lastCycleTimeStamp = currentSystemTime;
-		
-		// Write the timestamp to the timestamp logfile
-		//try {
-		//	BufferedWriter writer = new BufferedWriter(new FileWriter(this.timeStampFile, true));
-		//	writer.append((new Long(simulationCycleTime)).toString());
-		//	writer.newLine();
-		//	writer.close();
-		//} catch (IOException e) {
-		//	// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//}
+
+		//Todo: Make this parse lines from a file (or randomly generate perceptions)
 
 		return litteralList;
 	}
@@ -148,123 +107,9 @@ public class SimpleJasonAgent extends AgArch {
 	public void act(ActionExec action) {
 		System.out.println(action.toString());
 
-		// Get the action term
-		//Structure actionTerm = action.getActionTerm();
-		
-		// Log the action
-		//getTS().getLogger().info("Agent " + getAgName() + " is doing: " + actionTerm);
-		//logger.log(Level.FINE, "MYAgent " + getAgName() + " is doing: " + actionTerm);
-		
-		// Define the action string
-		//String actionString = "";
-		
-		// Define terms for possible actions (should move these to private class parameters)
-		//Term left = Literal.parseLiteral("turn(left)");
-		//Term right = Literal.parseLiteral("turn(right)");
-		//Term go = Literal.parseLiteral("thrust(on)");
-		//Term stop = Literal.parseLiteral("thrust(off)");
-		//Term up = Literal.parseLiteral("thrust(up)");
-		//Term down = Literal.parseLiteral("thrust(down)");
-		//Term hover = Literal.parseLiteral("hover");
-		
-        
-		// Check what action is being performed, update actionString accordingly.
-		//if (actionTerm.equals(left)) {
-		//	actionString = "turn(left)";
-		//}
-		//else if (actionTerm.equals(right))
-		//	actionString = "turn(right)";
-		//else if (actionTerm.equals(go))
-		//	actionString = "thrust(on)";
-		//else if (actionTerm.equals(stop))
-		//	actionString = "thrust(off)";
-		//else if (actionTerm.equals(up))
-		//	actionString = "thrust(up)";
-		//else if (actionTerm.equals(down))
-		//	actionString = "thrust(down)";
-		//else if (actionTerm.equals(hover))
-		//	actionString = "hover";
-		
-		// Add the action to agentState
-		//agentState.addAction(actionString);
-		//System.out.println(actionString);
-		
 		// Set that the execution was OK and flag it as complete.
 		action.setResult(true);
 		actionExecuted(action);
 	}
 
-	/*
-	@Override
-	public boolean canSleep() {
-		return !this.checkForFreshPerception();
-	}
-
-	@Override
-	public boolean isRunning() {
-		return running;
-	}
-    
-	@Override
-	public void stop() {
-		running = false;
-	}*
-	
-	// a very simple implementation of sleep
-	/*
-	public void sleep() {
-		logger.log(Level.FINE, "Snoozing");
-		try {
-			Thread.sleep(100);					// TODO: Need to revisit this
-		} catch (InterruptedException e) {}
-	}*/
-	
-	/**
-	 * Send message to another agent (via simulated wifi).
-	 *
-	/*
-	@Override
-	public void sendMsg(Message m) throws Exception {
-		// Make sure sender parameter is set
-        if (m.getSender() == null)  m.setSender(getAgName());
-		
-        // Put the message in the wifi queue
-        this.agentState.setMsgOut(m.toString());
-	}*/
-	
-	/**
-	 * in case agent is sleeping
-	 * TODO: in case we get problems of agents not waking up on messages, this is what to use!!
-	 * */
-	/*
-	public void wakeAgent() {
-        wakeUpSense();
-    }*/
-
-    /*
-	@Override
-	public void broadcast(Message m) throws Exception {
-		m.setReceiver(broadcastID);
-		this.sendMsg(m);
-	}*/
-
-	/*
-	@Override
-	public void checkMail() {
-		Circumstance circ = getTS().getC();
-		Queue<String> messages = new LinkedList<String>();
-		messages = this.agentState.getMsgIn();
-		if (messages.isEmpty())
-			return;
-		for(String messageString:messages) {
-			try {
-				 Message currentMessage = Message.parseMsg(messageString);
-				 if (currentMessage.getReceiver().equals(broadcastID) || currentMessage.getReceiver().equals(this.getAgName()))
-					 circ.addMsg(currentMessage);
-			} catch(Exception e) {
-				e.printStackTrace(System.out);
-			}
-		}
-	}*
-*/
 }
